171ee3de7c78bbd5bfcac5bb4eff50419bc51d23
chrislee1996 <prepchrislee@gmail.com>
2022-05-20 16:11:29 -0400

    able to produce movie titles

--- src/components/movies/IndexMovies.js
+++ src/components/movies/IndexMovies.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect } from 'react'
 import { getMovies } from '../../api/movie'
-import { Card} from 'react-bootstrap'
+import { Card,Col,Row, ListGroup} from 'react-bootstrap'
+
+const cardContainerLayout = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row wrap'
+}
 
 const IndexMovies = (props) => {
-    const [content, setContent] = useState([])
+    const [contents, setContent] = useState([])
     // const [numOfPages, setNumofPages] = useState()
     const [updated, setUpdated] = useState(false)
 
@@ -12,26 +18,40 @@ const IndexMovies = (props) => {
             const fetchData = async () => {
                 getMovies()
                 .then((res) => {
-                    setContent(res.data)
-                    console.log(res.data)
+                    setContent(res.data.results)
+                    console.log(res.data.results)
                 })
             }
             fetchData()
     },[updated])
 
+    if (!contents) {
+        return <span class ='loader'></span>
+    }
 
     let movieCards
 
-    // if (content.length > 0) {
-    //     movieCards = content.map(contents => (
-    //     {contents}
-    //     ))
-    // }
+    if (contents.length > 0) {
+        movieCards = contents.map((content) => (
+            <ListGroup.Item key = {content.title} style={{ width: '30%', border:"solid 1px"}} className="m-2 bg-dark text-info"  >
+                <Row style={{ alignItems:'center', color:'red'}}>
+                    <Col>
+                        {content.title}
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+        ))
+    }
+
+
 
     return (
-        <div>
-
-        </div>
+        <>
+            <h3 class='text-center text-info'>Movies</h3>
+            <div style={cardContainerLayout}>
+                {movieCards}
+            </div>
+        </>
     )
 }
 
