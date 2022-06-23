653af97994125baab9e3552453297795000f1cce
chrislee1996 <prepchrislee@gmail.com>
2022-06-23 18:30:34 -0400

    deployent

--- src/api/movie.js
+++ src/api/movie.js
@@ -4,9 +4,8 @@ import env from 'react-dotenv'
 
 
 export const getMovies = (page) => {
-    console.log('testtttttt123123')
     const searchUrl = 
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=1e82814411d52914e0401e0ea118c4d8&language=en-US&page=${page}`
     const config = {
         method: 'get',
         url:searchUrl
